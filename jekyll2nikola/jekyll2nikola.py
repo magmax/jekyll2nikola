9d3c8c9577e4834940bfdca0804a6fc6a10c2e70
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-12-08 16:28:51 +0100

    Another fix...

--- jekyll2nikola/jekyll2nikola.py
+++ jekyll2nikola/jekyll2nikola.py
@@ -40,8 +40,8 @@ class IntelligentMeta(object):
         if isinstance(metadata, list):
             self._meta = {}
             for item in metadata:
-                keys = item.keys()
-                key = keys[0] if isinstance(keys, list) else next(keys)
+                keys = list(item.keys())
+                key = keys[0]
                 value = item[key]
                 self._meta[key] = value
         elif isinstance(metadata, dict):
